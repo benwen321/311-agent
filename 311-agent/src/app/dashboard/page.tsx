7e29d9afb6ef9b2c3975c182745d994042b851e5
benwen321 <109177643+benwen321@users.noreply.github.com>
2025-05-25 20:00:47 -0700

    Added protecting to dashboard

--- 311-agent/src/app/dashboard/page.tsx
+++ 311-agent/src/app/dashboard/page.tsx
@@ -1,4 +1,12 @@
-export default function() {
+import { auth } from "@/auth"
+import { redirect } from 'next/navigation'
+
+export default async function Dashboard() {
+    const session = await auth()
+
+    if (!session) {
+        redirect("/api/auth/signin")
+    }
     return (
         <div> 
             <text>dashboard</text>
